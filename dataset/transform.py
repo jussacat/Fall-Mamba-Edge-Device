91b81386e265769fd77cfd09a39d622dc09ba637
uzine <phamnhung71697@gmail.com>
2026-08-25 15:07:44 +0700

    Update transform

--- dataset/transform.py
+++ dataset/transform.py
@@ -1,12 +1,12 @@
 import torch
-from torchvision import transform
+from torchvision import transforms
 
 def get_transform(is_train=True):
     base_transforms = [
-        transform.ToPILImage(),
-        transform.Resize((224, 224)), #Resnet input size
-        transform.ToTensor(),
-        transform.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) #ImageNet
+        transforms.ToPILImage(),
+        transforms.Resize((224, 224)), #Resnet input size
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) #ImageNet
     ]
 
-    return transform.Compose(base_transforms)
+    return transforms.Compose(base_transforms)
